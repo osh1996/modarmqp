aa2cceaca27a51bde286a25fcfe93bdf7f25ea9e
Alex <atags22@gmail.com>
2017-11-29 22:40:18 -0500

    Remove broken code

--- learnUI/main.py
+++ learnUI/main.py
@@ -1,6 +1,5 @@
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 import design
 
